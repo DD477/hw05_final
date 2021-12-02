05f8849cd6a99acf43c7f7c595434c62dca094b9
d.dobrodeev <d.dobrodeev@rambler-co.ru>
2021-12-02 21:44:38 +0300

    test pro-commit

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth import get_user_model
 
 from .forms import CommentForm, PostForm
-from .models import Follow, Group, Post
+from django.shortcuts import get_object_or_404, redirect, render
 
+from .models import Follow, Group, Post
 POSTS_QUANTITY = 10
 User = get_user_model()
 
